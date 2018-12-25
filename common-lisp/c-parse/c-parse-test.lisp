e8f1f25ddd9b8523412cdbc9f46ba80303755315
terminal625 <zpersoncool@gmail.com>
2018-12-25 14:45:49 -0500

    shuffling

--- common-lisp/c-parse/c-parse-test.lisp
+++ common-lisp/c-parse/c-parse-test.lisp
@@ -29,3 +29,86 @@
 		     (regex-character-class-to-esrap-liquid "[a-zA-Z_0-9]")
 		     "S"))
 	     #\S))
+
+(defun parse-with-garbage (rule text)
+  (c-parse-parse rule text :junk-allowed t))
+
+(defun test-parse-rules (&optional (rules *lex-patterns*))
+  (mapcar (lambda (text)
+	    (parse-with-garbage 'lex-rule-start text))
+	  rules))
+
+(define-c-parse-rule lex-def ()
+  (cap :def-name (v lex-token-string))
+  (v whitespace)
+  ;;(cap :rule (v lex-rule-start))
+  (stringify (postimes character)))
+
+;;need to evaluate this before testing
+(defparameter *defs* nil)
+(defun set-deps ()
+  (setf *defs*
+	(mapcar
+	 (lambda (item)
+	   (parse-with-garbage 'lex-def item))
+	 *lex-strings*)))
+
+;;run split-lex-2 to set the dynamic variables
+(defun test-lines (&optional (rule 'lex-rule-start) (rules *lex-patterns*))
+  (let ((correct 0)
+	(wrong 0))
+    (terpri)
+    (mapc (lambda (text)
+	    (let* ((obj (parse-with-garbage rule text))
+		   (a (princ-to-string 
+		       obj)))
+	      (flet ((dump ()
+		       (princ a)
+		       (terpri)
+		       (princ text)
+		       (terpri)))
+		(cond ((string-a-prefix-b-p
+			a
+			text)
+		       (progn
+			 (format t "~%same:~%") 
+			 (dump))
+		       (incf correct))
+		      (t
+		       (incf wrong)
+		       (format t "~%DIFFERENT:~%")
+		       (dump)
+	;	       (inspect obj)
+		       )))))
+	  rules)
+    (format t "correct: ~a wrong: ~a~%" correct wrong)
+    (list wrong correct)))
+
+(defun teststuff ()
+  (test-lines)
+  (test-lines 'lex-rule-start *defs*))
+
+;;(string-a-prefix-b-p "a" "ab") -> T
+;;(string-a-prefix-b-p "ac" "ab") -> 
+(defun string-a-prefix-b-p (a b)
+  "test whether string a is a prefix of b"
+  (when (> (length a)
+	   (length b))
+    ;;(error "a is longer than b")
+    (return-from string-a-prefix-b-p nil)
+    )
+  (dotimes (index (length a))
+    (unless (char= (aref a index)
+		   (aref b index))
+      (return-from string-a-prefix-b-p nil)))
+  t)
+
+(defun setup ()
+  (split-lex2)
+  (set-deps)
+  (values))
+
+(defun test-things (&optional not-pretty)
+  (let ((*print-raw* not-pretty))
+    (teststuff))
+  (values))
