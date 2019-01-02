330576f7a982df1c4e95af614fe19e902e6573c9
terminal625 <zpersoncool@gmail.com>
2019-01-02 01:53:44 -0500

    collecting those GCC and C11 oddities

--- common-lisp/c-parse/test/GNU_compiler_builtins.h
+++ common-lisp/c-parse/test/GNU_compiler_builtins.h
@@ -1,3 +1,16 @@
+//https://stackoverflow.com/questions/28983726/pycparser-not-working-on-preprocessed-code
 typedef struct __builtin_va_list { } __builtin_va_list;
+
 #define __attribute__(x)
-#define __extension__(x)
+#define __extension__
+//https://github.com/eliben/pycparser/issues/210
+#define __asm__(x)
+#define __const const
+#define __inline__ inline
+#define __inline inline
+#define __restrict
+#define __signed__ signed
+#define __GNUC_VA_LIST
+#define __gnuc_va_list char
+//https://stackoverflow.com/questions/3385515/static-assert-in-c
+#define _Static_assert(COND,MSG) 
